38bc96346105e2589be0064b2795d738aa4d7d88
Young Chen <young.chen@outlook.com>
2024-11-11 23:57:31 -0500

    Reformat files

--- hooks/use-game.ts
+++ hooks/use-game.ts
@@ -1,1 +1,21 @@
-export function useGame() { }
+import { useUser } from "./use-user";
+
+type Params = {
+    roomId: string
+}
+
+type Game = {
+    // State
+    isHost: boolean,
+    isPlayer: boolean,
+    isSpectator: boolean,
+    state: 'ready' | 'playing' | 'paused'
+    // Functions
+    start: () => void
+    pause: () => void
+    // Callbacks
+}
+
+export function useGame({ roomId }: Params) {
+    const { user } = useUser()
+}
--- pages/pong/lobby.tsx
+++ pages/pong/lobby.tsx
@@ -1,54 +1,80 @@
 import { NextPage } from "next";
-import { Badge, Button, Loading } from "@supabase/ui";
+import { Badge, Button, Input, Loading, Modal } from "@supabase/ui";
 import { useLatency } from "../../hooks/use-latency";
 import { useRouter } from "next/router";
 import { useCreateGame } from "../../hooks/use-create-game";
 import { useUser } from "../../hooks/use-user";
+import { useState } from "react";
 
 const Lobby: NextPage = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const { createGame, loading } = useCreateGame({
+    onCreated: (roomId: string) => {
+      router.push(`/pong/game/${roomId}`);
+    },
+  });
+  const [joinOpen, setJoinOpen] = useState(false);
+  const [joinInput, setJoinInput] = useState("");
+  const { user, signOut } = useUser();
+  const latency = useLatency();
 
-    const { createGame, loading } = useCreateGame({
-        onCreated: (roomId: string) => {
-            router.push(`/pong/game/${roomId}`)
-        }
-    })
+  return (
+    <div
+      className={[
+        "h-screen w-screen p-4 flex justify-center items-center relative",
+        "max-h-screen max-w-screen overflow-hidden",
+      ].join(" ")}
+    >
+      <Modal
+        size="tiny"
+        title="Join Game"
+        description="Enter the 6 digit room code"
+        visible={joinOpen}
+        onCancel={() => setJoinOpen(false)}
+        onConfirm={() => router.push(`/pong/game/${joinInput}`)}
+        confirmText="Join"
+      >
+        <Input
+          value={joinInput}
+          onChange={(e) =>
+            setJoinInput(
+              e.target.value
+                .toUpperCase()
+                .slice(0, 6)
+                .replaceAll(/[^A-Z0-9]]/g, "")
+            )
+          }
+        ></Input>
+      </Modal>
+      <div
+        className="absolute h-full w-full left-0 top-0 pointer-events-none"
+        style={{
+          opacity: 0.02,
+          backgroundSize: "16px 16px",
+          backgroundImage:
+            "linear-gradient(to right, gray 1px, transparent 1px),\n    linear-gradient(to bottom, gray 1px, transparent 1px)",
+        }}
+      />
+      <div className="flex flex-col flex-col-2 items-center gap-4 w-fit">
+        <h1 className="text-white text-6xl">PONG</h1>
+        {user ? (
+          <>
+            <Button block type="primary" size="xlarge" onClick={() => createGame(user.id)} loading={loading}>
+              Create
+            </Button>
+            <Button block type="default" size="xlarge" onClick={() => setJoinOpen(true)}>
+              Join
+            </Button>
+          </>
+        ) : (
+          <Loading active>{""}</Loading>
+        )}
+      </div>
+      <div className="absolute right-0 bottom-0 p-2" onDoubleClick={signOut}>
+        <Badge color="gray">{`${latency}ms // ${user?.id ?? ""}`}</Badge>
+      </div>
+    </div>
+  );
+};
 
-    const { user, signOut } = useUser()
-    const latency = useLatency()
-
-    return (
-        <div
-            className={[
-                'h-screen w-screen p-4 flex justify-center items-center relative',
-                'max-h-screen max-w-screen overflow-hidden',
-            ].join(' ')}
-        >
-            <div
-                className="absolute h-full w-full left-0 top-0 pointer-events-none"
-                style={{
-                    opacity: 0.02,
-                    backgroundSize: '16px 16px',
-                    backgroundImage:
-                        'linear-gradient(to right, gray 1px, transparent 1px),\n    linear-gradient(to bottom, gray 1px, transparent 1px)',
-                }}
-            />
-            <div className='flex flex-col flex-col-2 items-center gap-4 w-fit'>
-                <h1 className="text-white text-6xl">PONG</h1>
-                {
-                    user ? <>
-                        <Button block type='primary' size='xlarge' onClick={() => createGame(user.id)} loading={loading}>Create</Button>
-                        <Button block type='default' size='xlarge'>Join</Button>
-                    </>
-                        :
-                        <Loading active>Loading...</Loading>
-                }
-            </div>
-            <div className='absolute right-0 bottom-0 p-2' onDoubleClick={signOut}>
-                <Badge color="gray">{latency}ms // {user?.id}</Badge>
-            </div>
-        </div>
-    )
-}
-
-export default Lobby
+export default Lobby;
