7cdc5a57c0ca93dc868bd00cacda5e0752feac10
Young Chen <young.chen@outlook.com>
2024-11-17 15:24:26 -0500

    Fix image paths

--- components/pong/game/scenes/Preloader.ts
+++ components/pong/game/scenes/Preloader.ts
@@ -24,7 +24,7 @@ export class Preloader extends Scene {
 
   preload() {
     //  Load the assets for the game - Replace with your own assets
-    this.load.setPath("assets");
+    this.load.setPath("/assets");
 
     this.load.image("logo", "logo.png");
     this.load.image("star", "star.png");
